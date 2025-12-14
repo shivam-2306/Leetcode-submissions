4220171cd6d7dd396c224ef2f4a32b2452d2b1cf
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2025-12-14 11:38:23 +0530

    Time: 17 ms (86.00%) | Memory: 33.6 MB (91.50%) - LeetSync

--- 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.cpp
@@ -4,6 +4,10 @@ public:
         int result = 1, seatCounter = 0, totalSeats = 0;
         int mod = 1e9+7;
         int j = corridor.size()-1;
+        for(auto i:corridor){
+            if(i == 'S')totalSeats ++;
+        }
+        if(totalSeats == 0 || totalSeats % 2 == 1) return 0;
         while(j>= 0 && corridor[j] != 'S')j--;
         for(int i= 0;i<=j;i++){
             if(seatCounter == 2){
@@ -18,8 +22,6 @@ public:
             if(corridor[i] == 'S') {seatCounter++;totalSeats++;}
             
         }
-
-        if(totalSeats == 0 || seatCounter == 1) return 0;
         return result;
     }
 };
