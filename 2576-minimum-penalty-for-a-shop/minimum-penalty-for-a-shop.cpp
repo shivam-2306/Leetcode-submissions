401742c8ba88c2e0a38e7be2471609ad20ecfb59
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2025-12-26 12:15:24 +0530

    Time: 3391 ms (5.03%) | Memory: 21.9 MB (12.24%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+        vector<int> rightNCount(c.size()+1, 0), leftYCount(c.size()+1, 0);
+        for(int i = 0;i<c.length();i++){
+            rightNCount[i+1] = rightNCount[i];
+            if(c[i] == 'N'){
+                rightNCount[i+1]++;
+            }
+        }
+        for(int i = c.length()-1;i>=0;i--){
+            leftYCount[i] = leftYCount[i+1];
+            if(c[i] == 'Y'){
+                leftYCount[i]++;
+            }
+        }
+        int optimumIndex = 0;
+        int res = leftYCount[0]+rightNCount[0];cout<<res<<endl;
+        for(int i= 0 ;i<c.size()+1;i++){
+            int resTemp = res;
+            res = min(res, leftYCount[i]+rightNCount[i]);
+            cout<<res<<" "<<resTemp<<endl;
+            res != resTemp?optimumIndex = i:NULL;
+        }
+
+        //     cout<<resTemp<<" ";
+        //     res = min(res, leftYCount[i]+rightNCount[i]);
+        //     cout<<res<<endl;
+
+
+        // for(auto i:rightNCount){
+        //     cout<<i<< " ";
+        // }cout<<endl;
+        // for(auto i:leftYCount){
+        //     cout<<i<< " ";
+        // }cout<<endl;
+        return optimumIndex;
+        // return 0;
+    }
+};
