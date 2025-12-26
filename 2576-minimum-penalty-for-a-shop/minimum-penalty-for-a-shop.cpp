9f9b7aa2a030946270f242482611e550ca22579f
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2025-12-26 12:18:21 +0530

    Time: 22 ms (8.96%) | Memory: 21.6 MB (38.47%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     int bestClosingTime(string c) {
         vector<int> rightNCount(c.size()+1, 0), leftYCount(c.size()+1, 0);
-        for(int i = 0;i<c.length();i++){
-            rightNCount[i+1] = rightNCount[i];
-            if(c[i] == 'N'){
-                rightNCount[i+1]++;
-            }
-        }
         for(int i = c.length()-1;i>=0;i--){
             leftYCount[i] = leftYCount[i+1];
             if(c[i] == 'Y'){
@@ -15,13 +9,19 @@ public:
             }
         }
         int optimumIndex = 0;
-        int res = leftYCount[0]+rightNCount[0];cout<<res<<endl;
-        for(int i= 0 ;i<c.size()+1;i++){
+        int res = leftYCount[0]+rightNCount[0];
+        for(int i = 0;i<c.length();i++){
+            rightNCount[i+1] = rightNCount[i];
+            if(c[i] == 'N'){
+                rightNCount[i+1]++;
+            }
             int resTemp = res;
             res = min(res, leftYCount[i]+rightNCount[i]);
-            cout<<res<<" "<<resTemp<<endl;
             res != resTemp?optimumIndex = i:NULL;
         }
+        int resTemp = res;
+            res = min(res, leftYCount[c.size()]+rightNCount[c.size()]);
+            res != resTemp?optimumIndex = c.size():NULL;
 
         //     cout<<resTemp<<" ";
         //     res = min(res, leftYCount[i]+rightNCount[i]);
