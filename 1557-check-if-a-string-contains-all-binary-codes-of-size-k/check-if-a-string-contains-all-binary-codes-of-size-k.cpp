02f6c06cd7ce4f344f2c9351a8ed161822ab995c
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-02-23 15:40:25 +0530

    Time: 20 ms (90.76%) | Memory: 23.3 MB (95.11%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,43 +1,23 @@
-// class Solution {
-// public:
-//     bool hasAllCodes(string s, int k) {
-//         vector<bool> array (pow(2, k)-1, 0);
-//         string temp  = s.substr(0, k);
-//         int i = 0,j = k;
-//         while(j< s.size()){
-//             cout<<i<<endl;
-//             cout<<temp<<endl;
-//             cout<<stoi(temp)<<endl<<endl;
-//             array[stoi(temp)] = true;
-//             temp[i] = '0';
-//             i++;
-//             temp += s[j];
-//             j++;
-//         }
-//         for(auto i: array){
-//             if(i == false) return false;
-//         }
-//         return true;
-//     }
-// };
-
-#include <string>
-#include <unordered_set>
-
 class Solution {
 public:
-    bool hasAllCodes(std::string s, int k) {
-        // Edge case: string is shorter than the required code length
+    bool hasAllCodes(string s, int k) {
         if (s.size() < k) return false;
-        
-        std::unordered_set<std::string> seen;
-        
-        // Slide a window of size k across the string
-        for (int i = 0; i <= s.size() - k; i++) {
-            seen.insert(s.substr(i, k));
+
+        int total = 1 << k;
+        vector<bool> seen(total, false);
+
+        int hash = 0;
+        int mask = total - 1;
+
+        for (int i = 0; i < s.size(); i++) {
+            hash = ((hash << 1) & mask) | (s[i] - '0');
+
+            if (i >= k - 1 && !seen[hash]) {
+                seen[hash] = true;
+                total--;
+                if (total == 0) return true;
+            }
         }
-        
-        // Check if we found 2^k unique combinations
-        return seen.size() == (1 << k); 
+        return false;
     }
 };
