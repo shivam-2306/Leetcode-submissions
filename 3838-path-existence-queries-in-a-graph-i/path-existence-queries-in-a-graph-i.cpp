6a771ae75d49490f563fae7f3d4e6e13c1197a29
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-07-09 23:58:21 +0530

    Time: 266 ms (6.57%) | Memory: 309.1 MB (5.48%) - LeetSync

---
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        unordered_map<int,int> m;
+        m[0] = 1;
+        cout<<nums[0]<<"\n";
+        int graph = 1;
+        for(int i = 1;i<n;i++){
+            if(nums[i]-nums[i-1] <= maxDiff){
+                // cout<<"if\n";
+                m[i-1] = graph;
+                m[i] = graph;
+            }
+            else {
+                // cout<<nums[0];
+                // cout<<"else\n";
+                graph++;
+                m[i] = graph;
+            }
+        }
+        vector<bool> res;
+        for(auto i:queries){
+            // cout<<m[i[0]]<<" "<<m[i[1]]<<"\n";
+            res.push_back(m[i[0]] == m[i[1]]);
+        }
+        return res;
+    }
+};
