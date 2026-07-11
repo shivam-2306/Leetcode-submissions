bb34d193eeae93fb72fecf34b8f0144adcbb6379
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-07-11 13:40:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.60%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -2,15 +2,10 @@ class Solution {
 public:
     int numTrees(int n) {
         long long int res = 1;
-        int div = 1e9+7;
-        unsigned long long int num = 1;
-         unsigned long long int den = 1;
         for(int i = 1;i<=n;i++){
            res = res * (n + i) / i;
         }
         res /= (n+1);
-        den%=div;
-        cout<<num<<" "<<den;
         return res;
     }
 };
