db60de8661fd6e579c6408083a504569ff2bea9a
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-07-15 09:01:56 +0530

    Time: 1026 ms (52.17%) | Memory: 409.8 MB (9.56%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    int dp[201][201][201];
-    int mod = 1e9+7;
+    vector<vector<vector<int>>> dp;
+    int mod = 1e9 + 7;
     int n = 0;
-    int gcd (int a, int b){
-        while(b!=0){
-            int temp = a%b;
+
+    int gcd(int a, int b) {
+        while (b) {
+            int t = a % b;
             a = b;
-            b = temp;
+            b = t;
         }
         return a;
     }
-    int recursive(vector<int>& nums, int index, int gcd1, int gcd2){
-        if(index == n){
-            return (gcd1 == gcd2 && gcd1 & gcd2);
-        }
-        if(dp[index][gcd1][gcd2] != -1){
+
+    int recursive(vector<int>& nums, int index, int gcd1, int gcd2) {
+        if (index == n)
+            return (gcd1 == gcd2 && gcd1);
+
+        if (dp[index][gcd1][gcd2] != -1)
             return dp[index][gcd1][gcd2];
-        }
-        int res1 = recursive(nums, index+1, gcd(gcd1,nums[index]), gcd2);
-        int res2 = recursive(nums, index+1, gcd1, gcd(gcd2, nums[index]));
-        int res3 = recursive(nums, index+1, gcd1, gcd2);
-        dp[index][gcd1][gcd2] = ((res1+res2)%mod+res3)%mod;
-        return dp[index][gcd1][gcd2];
+
+        int res1 = recursive(nums, index + 1, gcd(gcd1, nums[index]), gcd2);
+        int res2 = recursive(nums, index + 1, gcd1, gcd(gcd2, nums[index]));
+        int res3 = recursive(nums, index + 1, gcd1, gcd2);
+
+        return dp[index][gcd1][gcd2] =
+            ((res1 + res2) % mod + res3) % mod;
     }
+
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
         n = nums.size();
-        return recursive(nums, 0, 0, 0);        
+
+        dp.assign(
+            n + 1,
+            vector<vector<int>>(
+                201,
+                vector<int>(201, -1)
+            )
+        );
+
+        return recursive(nums, 0, 0, 0);
     }
 };
