c60b17cfe1020d750022f847c34d616e2a384e38
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-07-24 09:27:07 +0530

    Time: 253 ms (95.51%) | Memory: 53.4 MB (60.67%) - LeetSync

--- 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.cpp
@@ -1,1 +1,27 @@
-class Solution { public: int uniqueXorTriplets(vector<int>& nums) { const int MAXX = 2048; vector<bool> pairXor(MAXX, false); vector<bool> ans(MAXX, false); for (int i = 0; i < nums.size(); i++) { for (int j = i; j < nums.size(); j++) { pairXor[nums[i] ^ nums[j]] = true; } } for (int x = 0; x < MAXX; x++) { if (!pairXor[x]) continue; for (int num : nums) { ans[x ^ num] = true; } } int res = 0; for (bool b : ans) res += b; return res; } };
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int MAXX = 2049;
+        vector<int> pairXorr(MAXX, 0);
+        vector<int> ansXorr(MAXX, 0);
+        for(int i = 0;i<nums.size();i++){
+            for(int j = i;j<nums.size();j++){
+                pairXorr[nums[i]^nums[j]] = true;
+            }
+        }
+
+        for(int i = 0;i<MAXX;i++){
+            if(pairXorr[i]){
+                for(auto j:nums){
+
+                ansXorr[i^j] = 1;
+                }
+            }
+        }
+        int res = 0;
+        for(auto i : ansXorr){
+            res+=i;
+        }
+        return res;
+    }
+};
