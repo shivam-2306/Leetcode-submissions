e412d22559c19835e3c93cca21bf9fa8dfad2820
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-07-31 09:26:05 +0530

    Time: 5 ms (79.89%) | Memory: 25.3 MB (94.44%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> frequencyMap(26, 0);
+        for(auto i:word){
+            frequencyMap[i-'a']++;
+        }
+        sort(frequencyMap.begin(), frequencyMap.end());
+        int covered = 0;
+        int res = 0;
+        // for(auto i: frequencyMap){
+        //     cout<<i<<" ";
+        // }
+        for (int j = 25;j>=0;j--){
+            if(frequencyMap[j] == 0) break;
+            if(covered < 8){
+                res += frequencyMap[j];
+                covered++;
+            }
+            else if(covered >= 8 && covered < 16){
+                res += frequencyMap[j]*2;
+                covered++;
+            }
+            else if(covered>=16 && covered<24) {
+                res+= frequencyMap[j]*3;
+                covered++;
+            }
+            else {
+                res += frequencyMap[j]*4;
+            }
+        }
+        return res;
+    }
+};
