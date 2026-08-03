26c147e2fadbcac74ec57831ed755ae079109593
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-08-04 00:07:56 +0530

    Time: 14 ms (83.23%) | Memory: 136.1 MB (83.46%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.cpp
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int calculate(vector<int>& stoneValue, int i, vector<int>& dp){
-        int n = stoneValue.size();
-        if(i == n){
-            return 0;
-        }
-        
-        if(dp[i] != -1e9){
-            return dp[i];
-        }
-        
-        int sum = 0;
-        int best = -1e9;
-        
-        for(int k = 0; k < 3 && i + k < n; k++){
-            sum += stoneValue[i + k];
-            best = max(best, sum - calculate(stoneValue, i + k + 1, dp));
-        }
-        
-        return dp[i] = best;
-    }
-
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n, -1e9);
-        
-        int maxi = calculate(stoneValue, 0, dp);
-        
-        if(maxi > 0) return "Alice";
-        if(maxi == 0) return "Tie";
-        return "Bob";
+
+        vector<int> dp(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int sum = 0;
+            dp[i] = INT_MIN;
+
+            for (int k = 0; k < 3 && i + k < n; k++) {
+                sum += stoneValue[i + k];
+                dp[i] = max(dp[i], sum - dp[i + k + 1]);
+            }
+        }
+
+        if (dp[0] > 0)
+            return "Alice";
+        if (dp[0] < 0)
+            return "Bob";
+        return "Tie";
     }
 };
