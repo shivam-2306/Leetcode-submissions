94915aed9a7ea2a3662c1bb065216b27cf710088
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-08-04 09:11:15 +0530

    Time: 19 ms (12.57%) | Memory: 19.7 MB (18.54%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int calculate(vector<int>& stoneValue, int i, int j, vector<vector<int> >& dp){
+    int maximum(vector<int>& piles, int i, int j, vector<vector<int> >& dp){
         if(i == j){
-            return stoneValue[i];
+            return piles[i];
         }
-        if(dp[i][j] != INT_MIN){
+        if(dp[i][j]!=INT_MIN){
             return dp[i][j];
         }
-        int sumRight = stoneValue[i] - calculate(stoneValue, i+1, j, dp);
-        int sumLeft = stoneValue[j] - calculate(stoneValue, i, j-1, dp);
+        int sumRight = piles[i] - maximum(piles, i+1,j, dp);
+        int sumLeft = piles[j] - maximum(piles, i, j-1, dp);
         return dp[i][j] = max(sumRight, sumLeft);
     }
-    bool stoneGame(vector<int>& stoneValue) {
-        // vector<vector<int> > dp(stoneValue.size(), vector<int> (stoneValue.size(), INT_MIN));
-        // int maxi = calculate(stoneValue, 0, stoneValue.size()-1,dp );
-        return true;
+    bool stoneGame(vector<int>& piles) {
+        vector<vector<int> > dp(piles.size(), vector<int> (piles.size(), INT_MIN));
+        int maxi = maximum(piles, 0, piles.size()-1, dp);
+        return maxi>0;
     }
 };
