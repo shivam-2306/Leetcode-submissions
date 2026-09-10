a4e11d5e2c722858efeb0c8cc718b075506d558f
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-09-10 15:13:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (18.47%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -9,18 +9,15 @@ public:
             1000000000ULL,
             1000000000000ULL,
             1000000000000000ULL,
-            1000000000000000000ULL
+            10000000000000000ULL
         };
 
         ull value = 0;
-
         for (int i = 0; i < 5; i++) {
             if (n < arr[i])
                 break;
-
             ull val = min<ull>(n, arr[i + 1] - 1);
             val -= arr[i]-1;
-
             value += val * (i + 1);
         }
 
