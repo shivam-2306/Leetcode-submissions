5b3a0792196b44183a9f34af37665bc554a20590
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-09-15 23:12:58 +0530

    Time: 82 ms (49.46%) | Memory: 258 MB (49.23%) - LeetSync

--- 4328-count-shadow-pairs-i/count-shadow-pairs-i.cpp
+++ 4328-count-shadow-pairs-i/count-shadow-pairs-i.cpp
@@ -3,31 +3,35 @@ public:
     long long shadowPairs(vector<int>& nums) {
         long long ans = 0;
         stack<pair<int, int>> s;
-        long long total = 0;
 
-        for (int x : nums) {
-            while (!s.empty() && s.top().first > x) {
-                total -= s.top().second;
-                s.pop();
-            }
+        for (auto i : nums) {
 
-            if (!s.empty()) {
-
-                if (s.top().first == x) {
-                    ans += total - s.top().second;
-                } 
-                else {
-                    ans += total;
-                }
-            }
-            if (!s.empty() && s.top().first == x) {
-                s.top().second++;
+            if (s.empty()) {
+                s.push({i, 1});
             } 
             else {
-                s.push({x, 1});
-            }
 
-            total++;
+                while (!s.empty() && s.top().first > i) {
+                    s.pop();
+                }
+
+                if (!s.empty()) {
+
+                    if (s.top().first != i) {
+                        ans += s.size();
+                    } 
+                    else {
+                        ans += s.size() - s.top().second;
+                    }
+                }
+
+                if (!s.empty() && s.top().first == i) {
+                    s.push({i, s.top().second + 1});
+                } 
+                else {
+                    s.push({i, 1});
+                }
+            }
         }
 
         return ans;
