2d38245aaa1c92d8e0302c0374018b35e3487944
shivam-2306 <101705435+shivam-2306@users.noreply.github.com>
2026-09-16 09:58:34 +0530

    Time: 446 ms (9.95%) | Memory: 332.8 MB (7.35%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,67 +1,67 @@
 class Solution {
 public:
-    int amountOfTime(TreeNode* root, int start) {
-        unordered_map<TreeNode*, TreeNode*> parent;
-        TreeNode* startNode = nullptr;
+    int startVal = 0;
+    TreeNode* start = nullptr;
 
-        // Build parent pointers and find start node
-        queue<TreeNode*> q;
-        q.push(root);
-        parent[root] = nullptr;
+    void adjMaker(unordered_map<TreeNode*, vector<TreeNode*>>& adj,
+                  TreeNode* root) {
+        if (root == nullptr)
+            return;
 
-        while (!q.empty()) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            if (node->val == start)
-                startNode = node;
-
-            if (node->left) {
-                parent[node->left] = node;
-                q.push(node->left);
-            }
-
-            if (node->right) {
-                parent[node->right] = node;
-                q.push(node->right);
-            }
+        if (root->val == startVal) {
+            start = root;
         }
 
-        // BFS from start
-        unordered_set<TreeNode*> visited;
-        q.push(startNode);
-        visited.insert(startNode);
+        if (root->left != nullptr) {
+            adj[root].push_back(root->left);
+            adj[root->left].push_back(root);
+        }
 
-        int time = -1;
+        if (root->right != nullptr) {
+            adj[root].push_back(root->right);
+            adj[root->right].push_back(root);
+        }
+
+        adjMaker(adj, root->left);
+        adjMaker(adj, root->right);
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+
+        unordered_map<TreeNode*, vector<TreeNode*>> adj;
+        unordered_set<TreeNode*> visited;
+
+        startVal = start;
+        adjMaker(adj, root);
+
+        queue<TreeNode*> q;
+
+        q.push(this->start);
+        visited.insert(this->start);
+
+        int res = -1;
 
         while (!q.empty()) {
-            int sz = q.size();
-            time++;
 
-            while (sz--) {
-                TreeNode* node = q.front();
+            int size = q.size();
+            res++;
+
+            while (size--) {
+
+                TreeNode* temp = q.front();
                 q.pop();
 
-                // Left
-                if (node->left && !visited.count(node->left)) {
-                    visited.insert(node->left);
-                    q.push(node->left);
-                }
+                for (TreeNode* node : adj[temp]) {
 
-                // Right
-                if (node->right && !visited.count(node->right)) {
-                    visited.insert(node->right);
-                    q.push(node->right);
-                }
+                    if (visited.find(node) != visited.end())
+                        continue;
 
-                // Parent
-                if (parent[node] && !visited.count(parent[node])) {
-                    visited.insert(parent[node]);
-                    q.push(parent[node]);
+                    visited.insert(node);
+                    q.push(node);
                 }
             }
         }
 
-        return time;
+        return res;
     }
 };
